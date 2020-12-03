6d6282265c86704870e78b61893b6ee2f2f328ea
Taras-SH13 <60879906+Taras-SH13@users.noreply.github.com>
2020-12-03 16:18:04 +0200

    Lesson 9 complete

--- p0091_onclickbuttons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
+++ p0091_onclickbuttons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.p0091_onclickbuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // найдем Ve
+        // найдем View элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+    // создаем обработчик нажатия
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //TODO Auto-generated method stub
+                tvOut.setText("Нажата кнопка OK");
+            }
+        };
+        View.OnClickListener oclBtnCancel= v -> {
+            //меняем текст в TextVie (tvOut)
+            tvOut.setText("Нажата кнопка Cancel");
+        };
+        // присвоим обработчик кнопке ОК
+        btnOk.setOnClickListener(oclBtnOk);
+        // присвоим обработчик кнопке Cancel
+        btnCancel.setOnClickListener(oclBtnCancel);
     }
 }
